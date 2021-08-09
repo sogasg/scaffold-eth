3d0ac9b2be86774e5b3e5c741be8335788437ff1
sogasg <sogasg@gmail.com>
2021-08-09 22:42:33 +0200

    Challenge 1: Decentralized Staking App - Checkpoints 1-4

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,30 +1,68 @@
 pragma solidity >=0.6.0 <0.7.0;
 
-import "hardhat/console.sol";
-import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+interface ExternalExampleContract {
+	function complete() external payable;
+}
 
 contract Staker {
+	mapping(address => uint256) public balances;
+	uint256 public constant threshold = 1 ether;
+	uint256 public deadline = now + 30 seconds;
+	address immutable externalExampleContract;
+	bool disableAndActivateWithdrawals = false;
+	bool firstRun = true;
 
-  ExampleExternalContract public exampleExternalContract;
+	event Stake(address staker, uint256 amount);
 
-  constructor(address exampleExternalContractAddress) public {
-    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+	constructor(address _externalExampleContract) public {
+		externalExampleContract = _externalExampleContract;
+	}
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+	modifier beforeDeadline() {
+		require(timeLeft() > 0, "deadline not passed");
+		_;
+	}
 
+	modifier afterDeadline() {
+		require(timeLeft() == 0, "deadline is NOT passed");
+		_;
+	}
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+	modifier contractIsActive() {
+		require(!disableAndActivateWithdrawals, "contract is disabled");
+		_;
+	}
 
+	modifier contractIsDisabled() {
+		require(disableAndActivateWithdrawals, "contract is active");
+		_;
+	}
 
+	function stake() public payable contractIsActive {
+		balances[msg.sender] += msg.value;
+		require(balances[msg.sender] >= msg.value);
+		emit Stake(msg.sender, msg.value);
+	}
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+	function execute() public afterDeadline contractIsActive {
+		if (address(this).balance >= threshold) {
+			ExternalExampleContract(externalExampleContract).complete{
+				value: address(this).balance
+			}();
+		} else if (firstRun) {
+			disableAndActivateWithdrawals = true;
+		}
+		firstRun = false;
+	}
 
+	function withdraw(address payable to) public contractIsDisabled {
+		require(balances[to] > 0);
+		uint256 amount = balances[to];
+		balances[to] = 0;
+		payable(to).transfer(amount);
+	}
 
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
+	function timeLeft() public view returns (uint256) {
+		return now < deadline ? deadline - now : 0;
+	}
 }
